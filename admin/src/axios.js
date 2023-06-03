f53d7dc4ce1ad4f077788c07c3b03c132960d69e
Success-akinyemi <brownsamuel915@gmail.com>
2023-06-03 03:52:30 -0700

    change of server url from localhost to live in client and admin

--- admin/src/axios.js
+++ admin/src/axios.js
@@ -1,7 +1,10 @@
 import axios from 'axios'
 
 //const BASE_URL: 'http://localhost:5000'
+//const BAS
 
 export const request = axios.create({
-    baseURL: 'http://localhost:5000'
+    baseURL: 'https://happyabode.onrender.com'
+    //https://happyabode.onrender.com
+    //http://localhost:5000
 })
--- client/src/axios.js
+++ client/src/axios.js
@@ -3,5 +3,7 @@ import axios from 'axios'
 //const BASE_URL: 'http://localhost:5000'
 
 export const request = axios.create({
-    baseURL: 'http://localhost:5000'
+    baseURL: 'https://happyabode.onrender.com'
+    //https://happyabode.onrender.com
+    //http://localhost:5000
 })
